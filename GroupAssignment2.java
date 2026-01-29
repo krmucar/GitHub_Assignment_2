ad0b4debfee45eace364f577e1f7c011d656359e
mehmet halim <233568706+mhmthlm@users.noreply.github.com>
2026-01-29 20:56:17 +0300

    Implementation of part d
    Average Difference

--- GroupAssignment2.java
+++ GroupAssignment2.java
@@ -57,9 +57,30 @@ public class GroupAssignment2 {
         
         return sum - sumOfEvenNumbers(arr1);
     }
-    
 
-    
+    public static void findAvrgDiff(int[] arr){
+
+        int total = 0;
+        for(int i=0; i<arr.length; i++){
+            total = arr[i] + total;
+        }
+            
+        int avrg = total/arr.length;
+        int[] newArr = new int[arr.length];
+        for(int i=0; i<arr.length; i++){
+            newArr[i] = arr[i] - avrg;
+                
+        }
+        System.out.print("{");
+        int count = 0;
+        for(int i=0; i<newArr.length-1; i++){
+            count++;
+            System.out.print(newArr[i] + "," + " ");
+        }
+        System.out.print(arr[count]);
+        System.out.print("}");
+    }
+     
 }
 
     
